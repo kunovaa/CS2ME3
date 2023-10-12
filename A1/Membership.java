f4f6f73dc76dbb2ba988cb0962a8c1d4a8aa1778
kunovaa <122912614+kunovaa@users.noreply.github.com>
2023-10-12 15:15:09 -0400

    Update

---
+++ A1/Membership.java
@@ -0,0 +1,15 @@
+
+// Membership Class
+public abstract class Membership {
+    
+    private String type;
+    private double discount;
+
+    public string getType() {
+        return type;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+}
