3dcfd00252cefefe4227ba2777ebdf8a7aee1eb1
kunovaa <122912614+kunovaa@users.noreply.github.com>
2023-10-29 19:26:00 -0400

    Update

--- A1/ReservationREPL.java
+++ A1/ReservationREPL.java
@@ -1,39 +1,166 @@
 import java.util.Scanner;
 
 public class ReservationREPL {
-    public class Booking {
-        private int bookingId;
-
-        public Booking(int bookingId) {
-            this.bookingId = bookingId;
+    public enum CodeGeneration {
+        REGULAR(0),
+        SILVER(10),
+        GOLD(20);
+    
+        private final int discountPercentage;
+    
+        CodeGeneration(int discountPercentage) {
+            this.discountPercentage = discountPercentage;
+        }
+    
+        public int getDiscountPercentage() {
+            return discountPercentage;
         }
     }
-
-    public class Car {
+    
+    public class Client {
+        private int clientID;
+        private String name;
+        private String contactInfo;
+        private CodeGeneration membershipType;
+    
+        public Client(int clientID, String name, String contactInfo, CodeGeneration membershipType) {
+            this.clientID = clientID;
+            this.name = name;
+            this.contactInfo = contactInfo;
+            this.membershipType = membershipType;
+        }
+    
+        public int getClientID() {
+            return clientID;
+        }
+    
+        public String getName() {
+            return name;
+        }
+    
+        public String getContactInfo() {
+            return contactInfo;
+        }
+    
+        public CodeGeneration getMembershipType() {
+            return membershipType;
+        }
+    
+        public void setMembershipType(CodeGeneration membershipType) {
+            this.membershipType = membershipType;
+        }
+    
+    }
+    
+    public class Booking {
+        private int bookingID;
+        private String startDate;
+        private String endDate;
+        private double totalCost;
+    
+        public Booking(int bookingID, String startDate, String endDate, double totalCost) {
+            this.bookingID = bookingID;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.totalCost = totalCost;
+        }
+    
+        public int getBookingID() {
+            return bookingID;
+        }
+    
+        public String getStartDate() {
+            return startDate;
+        }
+    
+        public String getEndDate() {
+            return endDate;
+        }
+    
+        public double getTotalCost() {
+            return totalCost;
+        }
+    
+        public double calculateTotalCost() {
+            // Calculation for total cost
+            return totalCost;
+        }
+    
+    }
+    
+    public class Service {
+        private String serviceType;
+    
+        public Service(String serviceType) {
+            this.serviceType = serviceType;
+        }
+    
+        public String getServiceType() {
+            return serviceType;
+        }
+    
+    }
+    public class Car extends Service {
         private String licensePlate;
         private int year;
         private String make;
         private int numberOfDoors;
-
-        public Car(String licensePlate, int year, String make, int numberOfDoors) {
+    
+        public Car(String serviceType, String licensePlate, int year, String make, int numberOfDoors) {
+            super(serviceType);
             this.licensePlate = licensePlate;
             this.year = year;
             this.make = make;
             this.numberOfDoors = numberOfDoors;
         }
+    
+        public String getLicensePlate() {
+            return licensePlate;
+        }
+    
+        public int getYear() {
+            return year;
+        }
+    
+        public String getMake() {
+            return make;
+        }
+    
+        public int getNumberOfDoors() {
+            return numberOfDoors;
+        }
     }
-
-    public class Vacation {
+    
+    public class Vacation extends Service {
         private String country;
         private String city;
         private String season;
-
-        public Vacation(String country, String city, String season) {
+        private double cost;
+    
+        public Vacation(String serviceType, String country, String city, String season, double cost) {
+            super(serviceType);
             this.country = country;
             this.city = city;
             this.season = season;
+            this.cost = cost;
         }
-    }
+    
+        public String getCountry() {
+            return country;
+        }
+    
+        public String getCity() {
+            return city;
+        }
+    
+        public String getSeason() {
+            return season;
+        }
+    
+        public double getCost() {
+            return cost;
+        }
+    }    
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
