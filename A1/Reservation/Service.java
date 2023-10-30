6b67c3c1a06b1114aa4112c8a32c21b95dbb6faa
Blaibelj <blaibelj@mcmaster.ca>
2023-10-30 19:20:54 -0400

    edited the cost

--- A1/Reservation/Service.java
+++ A1/Reservation/Service.java
@@ -2,12 +2,22 @@ package Reservation;
 
 public class Service {
     private String serviceType;
+    private double cost;
 
     public Service() {
     }
 
     public String getServiceType() {
         return serviceType;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double newcost) {
+        cost = newcost;
+
     }
 
 }
