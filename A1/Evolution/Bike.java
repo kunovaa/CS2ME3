868fe09bdfa44c7a5e108199b6b568d3d8ab3e04
kunovaa <122912614+kunovaa@users.noreply.github.com>
2023-10-30 19:38:20 -0400

    Bike added

---
+++ A1/Evolution/Bike.java
@@ -0,0 +1,25 @@
+package Evolution;
+
+public class Bike {
+    private String licensePlate;
+    private String make;
+    private int year;
+
+    public Bike(String licensePlate, String make, int year) {
+        this.licensePlate = licensePlate;
+        this.make = make;
+        this.year = year;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
